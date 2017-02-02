8a59fcb5ab794033eb396075c3795bf920559381
Michael Sefranek <msefran@gmail.com>
2017-02-02 16:50:45 -0600

    Web CSV adds rows now

--- WebCSVReport/routes.rb
+++ WebCSVReport/routes.rb
@@ -15,15 +15,33 @@ get "/account" do
 end
 
 
-# #end
-# 	get ("/Priya"){
-# 	build_accounts("Priya")
-# 		erb :csv_views
-# 	}
+post "/getinfo" do
+
+	CSV.open("accounts.csv", "a") do |csv|
+		binding.pry
+		formArray = params.values
+		formArray[4] = "$" + formArray[4]
+		formArray[5] = "$" + formArray[5]
+		formArray[1] = formArray[1].gsub(/[-]/,"/")
+		csv << formArray
+
+	end
+	
 
 
-# 	get ("/Sonia+Priya"){
-# 		build_accounts("bldrsdjvsldkvj")
-# 		erb :csv_views
-# 	}
+	
+	# binding.pry
+	# @csvRow = @account + "," + @date + "," + @payee + "," + @category + "," + @inflow + "," + @outflow
+	# binding.pry
+
+	# {name=> Priya, date=> 12412 ,Payee=> WALGREENS, Category=> Medical/Dental, etcetc}
+	#combine key values into one string with commas
+	#CSV.open("./accounts.csv","ab") do |csv| # write to end of CSV file
+end
+
+
+get ("/Sonia+Priya"){
+	build_accounts("bldrsdjvsldkvj")
+	erb :csv_views
+}
 
