4237e63052099be001c5d620415c29403e403322
Michael Sefranek <msefran@gmail.com>
2017-02-02 13:39:47 -0600

    got CSV Web to work with queries

--- WebCSVReport/routes.rb
+++ WebCSVReport/routes.rb
@@ -3,24 +3,27 @@ require "./functions.rb"
 require "CSV"
 require "pry"
 # http:/localhost:4567/ ...
-
-	get ("/Sonia"){
-	build_accounts("Sonia")
-
-#
-		erb :csv_views
-	}
-#end
-	get ("/Priya"){
-	build_accounts("Priya")
-		erb :csv_views
-	}
-
-
-	get ("/Sonia+Priya"){
-		build_accounts("bldrsdjvsldkvj")
-		erb :csv_views
-	}
-get("/home"){
+get "/home" do
 	erb :csv_home
-}
+end
+
+
+get "/account" do
+	@account_name = params
+	build_accounts(@account_name["name"])
+	erb :csv_views
+end
+
+
+# #end
+# 	get ("/Priya"){
+# 	build_accounts("Priya")
+# 		erb :csv_views
+# 	}
+
+
+# 	get ("/Sonia+Priya"){
+# 		build_accounts("bldrsdjvsldkvj")
+# 		erb :csv_views
+# 	}
+
