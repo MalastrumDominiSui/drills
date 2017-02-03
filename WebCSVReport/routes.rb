028044515c6af99ca317f642c264e72d460b281b
Michael Sefranek <msefran@gmail.com>
2017-02-02 22:22:51 -0600

    some solo work

--- WebCSVReport/routes.rb
+++ WebCSVReport/routes.rb
@@ -3,10 +3,10 @@ require "./functions.rb"
 require "CSV"
 require "pry"
 # http:/localhost:4567/ ...
+
 get "/home" do
 	erb :csv_home
 end
-
 
 get "/account" do
 	@account_name = params
@@ -14,34 +14,18 @@ get "/account" do
 	erb :csv_views
 end
 
-
 post "/getinfo" do
-
-	CSV.open("accounts.csv", "a") do |csv|
-		binding.pry
-		formArray = params.values
-		formArray[4] = "$" + formArray[4]
-		formArray[5] = "$" + formArray[5]
-		formArray[1] = formArray[1].gsub(/[-]/,"/")
-		csv << formArray
-
+	formArray = params.values # we tried to set this inside the if, but it seems better here because .include on the next line needs to work on an array (like formArray) rather than the hash (params)
+	if formArray.include?("") == false  # we were trying to catch nil, but empty params actually seem to return "", so this conditional is better
+		CSV.open("accounts.csv", "a") do |csv|
+			formArray[4] = "$" + formArray[4] # does the formArray had to begin with @? not sure
+			formArray[5] = "$" + formArray[5]
+			formArray[1] = formArray[1].gsub(/[-]/,"/")
+			csv << formArray # TODO : this somehow need to make a newLine so it doesnt keep appending to the same line
+		end
+	else
+		redirect "/home" # it might be nice to define a site here that throws an error?
 	end
-	
+	redirect "/account"
 
-
-	
-	# binding.pry
-	# @csvRow = @account + "," + @date + "," + @payee + "," + @category + "," + @inflow + "," + @outflow
-	# binding.pry
-
-	# {name=> Priya, date=> 12412 ,Payee=> WALGREENS, Category=> Medical/Dental, etcetc}
-	#combine key values into one string with commas
-	#CSV.open("./accounts.csv","ab") do |csv| # write to end of CSV file
 end
-
-
-get ("/Sonia+Priya"){
-	build_accounts("bldrsdjvsldkvj")
-	erb :csv_views
-}
-
