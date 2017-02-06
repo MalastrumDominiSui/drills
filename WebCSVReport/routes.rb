ca95d0164bc07a6c48a17cdffd2921ad395e5aee
Michael Sefranek <msefran@gmail.com>
2017-02-06 11:48:54 -0600

    added facebook bare files

--- WebCSVReport/routes.rb
+++ WebCSVReport/routes.rb
@@ -5,7 +5,11 @@ require "pry"
 # http:/localhost:4567/ ...
 
 get "/home" do
-	erb :csv_home
+	erb :home
+end
+
+get "/csv_addrow" do
+	erb :csv_addrow
 end
 
 get "/account" do
@@ -18,14 +22,25 @@ post "/getinfo" do
 	formArray = params.values # we tried to set this inside the if, but it seems better here because .include on the next line needs to work on an array (like formArray) rather than the hash (params)
 	if formArray.include?("") == false  # we were trying to catch nil, but empty params actually seem to return "", so this conditional is better
 		CSV.open("accounts.csv", "a") do |csv|
-			formArray[4] = "$" + formArray[4] # does the formArray had to begin with @? not sure
+			formArray[4] = "$" + formArray[4]
 			formArray[5] = "$" + formArray[5]
 			formArray[1] = formArray[1].gsub(/[-]/,"/")
-			csv << formArray # TODO : this somehow need to make a newLine so it doesnt keep appending to the same line
+			csv << formArray
 		end
 	else
-		redirect "/home" # it might be nice to define a site here that throws an error?
+		redirect "/csv_addrow" # it might be nice to define a site here that throws an error?
 	end
-	redirect "/account"
+	redirect "/account" 
+end
 
+post "/login" do
+	erb :csv_admin
+	@adminAccount = params[login]
+	@adminPassword = params[password]
+	params
+end
+
+post "/admin" do
+	## code
+	redirect "/csv_addrow"
 end
